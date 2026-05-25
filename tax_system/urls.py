8d4db392fef5997394c2b395edae38cf3465da05
Your Name <your.email@example.com>
2026-05-26 07:07:03 +1100

    Add homepage endpoint

--- tax_system/urls.py
+++ tax_system/urls.py
@@ -1,10 +1,13 @@
+from django.http import JsonResponse
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
-
+def home(request):
+    return JsonResponse({"message": "API is running"})
 urlpatterns = [
+    path("", home),
     path('admin/', admin.site.urls),
 
     # API v1
