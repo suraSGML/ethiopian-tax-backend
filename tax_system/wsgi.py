eb50462bd4cac60318dd18d4d66d8a98fa62ac33
Your Name <your.email@example.com>
2026-05-26 06:54:21 +1100

    Change wsgi.py to check env var before setting default

--- tax_system/wsgi.py
+++ tax_system/wsgi.py
@@ -1,5 +1,7 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tax_system.settings.production')
+# Don't set a default - rely on environment variable from render.yaml
+if 'DJANGO_SETTINGS_MODULE' not in os.environ:
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'tax_system.settings.production'
 application = get_wsgi_application()
